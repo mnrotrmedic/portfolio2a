bb66bb187390fe6ae31b8fb7d87512d40f99c713
mnrotrmedic <33576579+mnrotrmedic@users.noreply.github.com>
2019-04-14 21:38:59 -0500

    I like it so far

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,15 +1,11 @@
-function openPage(pageName, elmnt, color) {
+function openPage(pageName) {
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
     }
-    tablinks = document.getElementsByClassName("tablink");
-    for (i = 0; i < tablinks.length; i++) {
-        // tablinks[i].style.backgroundColor = "";
-    }
+
     document.getElementById(pageName).style.display = "block";
-    // elmnt.style.backgroundColor = color;
 }
 
 // Get the element with id="defaultOpen" and click on it
